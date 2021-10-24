44bb4c3069ed4c21f25bba1752b3375477e72e65
Steven Crutchfield <stevccru@icloud.com>
2021-10-24 13:56:52 -0400

    linked roducts from commerce api, still trying to fix images

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -9,16 +9,18 @@ const Product = ({ product}) => {
     const classes = useStyles();
 
 
+
+
     return (
         <Card className ={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title ={product.name}/>
+            <CardMedia className={classes.media} image={product.image.filename} title ={product.name}/>
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant ="h5" gutterBottom>
                      {product.name}
                     </Typography>
                     <Typography variant="h5">
-                    {product.price} 
+                    {product.price.formatted_with_symbol} 
                     </Typography>
                   
                 </div>
